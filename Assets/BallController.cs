a2fe029be948edac6726a3bbc592ada39a40677b
dkarakay <androidkarakay@gmail.com>
2018-06-04 14:18:50 +0300

    Files added

---
+++ Assets/BallController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+    public float speed = 100f;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Rigidbody2D>().velocity = Vector2.up * speed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        // Hit the Racket?
+        if (col.gameObject.name == "Racket")
+        {
+            // Calculate hit Factor
+            float x = hitFactor(transform.position,
+                              col.transform.position,
+                              col.collider.bounds.size.x);
+
+            // Calculate direction, set length to 1
+            Vector2 dir = new Vector2(x, 1).normalized;
+
+            // Set Velocity with dir * speed
+            GetComponent<Rigidbody2D>().velocity = dir * speed;
+        }else if(col.gameObject.name == "Bottom"){
+            SceneManager.LoadScene("MainScene");
+        }
+    }
+
+
+    float hitFactor(Vector2 ballPos, Vector2 racketPos,
+                float racketWidth){
+        // ascii art:
+        //
+        // 1  -0.5  0  0.5   1  <- x value
+        // ===================  <- racket
+        //
+        return (ballPos.x - racketPos.x) / racketWidth;
+    }
+
+}
---
+++ Assets/Scripts/RacketController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RacketController : MonoBehaviour {
+
+    //Movement Speed
+    public float speed = 150;
+
+
+	void FixedUpdate () {
+
+        float button = Input.GetAxisRaw("Horizontal");
+
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * button * speed;
+    }
+}
